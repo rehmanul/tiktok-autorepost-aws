92de936399d211d3d2c8d977677d73c7118f3c82
Your Name <you@example.com>
2025-10-28 02:39:12 +0600

    fix(web): configure static export

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: '10mb'
